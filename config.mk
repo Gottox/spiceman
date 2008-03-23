ed8b5b0c618014bfe04634a2feb32ebe59551a1d
Enno Boland (Gottox) <gottox@s01.de>
2008-03-23 21:12:16 +0100

    Added CACHEPREFIX constant.

--- config.mk
+++ config.mk
@@ -11,14 +11,17 @@ MANPREFIX = ${PREFIX}/share/man
 #DBPREFIX = ${PREFIX}/var/db/pkg
 # This is better for debugging
 DBPREFIX = .
+CACHEPREFIX = .
+
 
 # includes and libs
 INCS = -I. -I/usr/include
 LIBS = -L/usr/lib -lc
 
 # flags
-#CFLAGS = -static -Os -g -Wall -Werror ${INCS} -DVERSION=\"${VERSION}\" -DDEFREPO="\"${DEFREPO}\"" -DDBPREFIX=\"${DBPREFIX}\"
-CFLAGS = -O0 -g -Wall -Werror ${INCS} -DVERSION=\"${VERSION}\" -DDBPREFIX=\"${DBPREFIX}\" -DINITREPONAME=\"${INITREPONAME}\" -DINITREPOADDR=\"${INITREPOADDR}\"
+CFLAGS = -O0 -g -Wall -Werror ${INCS} -DVERSION=\"${VERSION}\" \
+	-DDBPREFIX=\"${DBPREFIX}\" -DCACHEPREFIX=\"${CACHEPREFIX}\" \
+	-DINITREPONAME=\"${INITREPONAME}\" -DINITREPOADDR=\"${INITREPOADDR}\"
 LDFLAGS = -g ${LIBS}
 
 # compiler and linker
